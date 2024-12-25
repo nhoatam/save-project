b2754e9f68991508f2437055520b6ea214185baa
nhoatam.dev <nhoatam.dev@gmail.com>
2024-12-25 16:05:38 +0700

    update config for publishing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { About } from "./Pages/About";
 import { Contact } from "./Pages/Contact";
 import { Home } from "./Pages/Home";
 import { NoPage } from "./Pages/NoPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Project } from "./Pages/Project";
 import { Service } from "./Pages/Service";
 import { AppProvider } from "./AppContext";
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <AppProvider>
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route index element={<Home />} />
             <Route path="/home" element={<Home />} />
@@ -21,7 +21,7 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NoPage />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </AppProvider>
     </>
   );
